6998894aa65876eea538dbea93432e804fb9a5f8
Cristal Davila <cristal.davila@gmail.com>
2023-09-13 14:53:13 -0600

    walks talks barks sits

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
-class Dog:
-    # Class body goes here
+# Class body goes here
 
-    #Instance method definition
-    pass
+class Dog:
+    def bark(self):
+        print("Woof!")
+
+
+#Instance method definition
+    def sit(self):
+        print("The dog is sitting.")
